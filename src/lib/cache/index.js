b2d24caff4aba6aa78798c75306ce551aa324ba2
Johan Kanefur <johan.canefur@gmail.com>
2016-10-27 11:11:13 +0200

    Caching layer (#3)
    * Added readthrough logic
    
    * Added redis dep
    
    * Added tests to readthrough cache
    
    * Separated readthrough cache from redis implementation
    
    * Removed test schemas

--- src/lib/cache/index.js
+++ src/lib/cache/index.js
@@ -1,6 +1,28 @@
+'use strict';
+
 /**
- * Read through cache implementation with Redis
+ * Contains the readthrough configuration
+ * @type {Object}
  */
+const settings = {};
+
+/**
+ * Read through cache implementation
+ * @param {Object} config Config object
+ */
+function setup(config) {
+  if (typeof config.write !== 'function') {
+    throw new TypeError('write property must be a function');
+  }
+
+  if (typeof config.read !== 'function') {
+    throw new TypeError('read property must be a function');
+  }
+
+  settings.read = config.read;
+  settings.write = config.write;
+  settings.enabled = 'enabled' in config ? config.enabled : true;
+}
 
 /**
  * Constructs a cache key for data
@@ -9,49 +31,70 @@
  * @return {string}            Constructed cache key
  */
 function constructCacheKey(entity, identifier) {
-  return 'entity_' + entity + '_' + JSON.stringify(identifier);
+  // Sort the keys if there are more than 1, to eliminiate duplicate keys
+  if (Object.keys(identifier).length > 1) {
+    const sortedKeys = Object.keys(identifier).sort();
+    const sortedIdentifier = {};
+
+    for (const key of sortedKeys) {
+      sortedIdentifier[key] = identifier[key];
+    }
+
+    identifier = sortedIdentifier;
+  }
+
+  // Create a key from JSON
+  return 'entity_' + entity + JSON.stringify(identifier)
+    .replace(/[\W_]+/g, '_');
 }
 
 /**
- * Fetches data from cache
- * @param  {string} key Cache key to load from
- * @return {Promise}    Data on success, null on fail
+ * Wraps the user specified callback in a promise
+ * @param {Function} callback Callback function
+ * @return {Promise}          Wrapped promise if needed
  */
-function fetchFromCache(key) {
-  return Promise.resolve(key);
+function getUserDataSource(callback) {
+  let result = callback();
+
+  if (!(result instanceof Promise)) {
+    result = Promise.resolve(result);
+  }
+
+  return result;
 }
 
 /**
- * Read through helper function
- * @param  {string}   entity     Name of the entity to get
- * @param  {object}   identifier Object that identifies the data
- * @param  {int}      ttl        Time-to-live for the object
- * @param  {Function} callback   Callback that collects data if it doesnt exists
- *                               in cache. Should return a Promise
- * @return {Promise}             Resolves the gathered data
+ * Readthrough
+ * @param {String}   entity     Name of the entity to cache (for generating key)
+ * @param {Object}   identifier Object to identify the entry
+ * @param {integer}  ttl        Time to live for the entry
+ * @param {Function} callback   Function to run when the data wasnt found
+ * @return {Promise}            Promise with resolved data
  */
 function readthrough(entity, identifier, ttl, callback) {
-  const cacheKey = constructCacheKey(entity, identifier);
+  if (!settings.enabled) {
+    return getUserDataSource(callback);
+  }
 
-  return fetchFromCache(cacheKey).then(function(data) {
-    // Early return if the data existed in cache
-    if (!(data === null)) {
-      return data;
+  const key = constructCacheKey(entity, identifier);
+  // Try to get the data from cache
+  return settings.read(key).then(function(data) {
+    if (!(data === undefined)) {
+      return JSON.parse(data);
     }
 
-    // Run the callback to get the data
-    let result = callback();
-
-    // Wrap scalar values in promise
-    if (!(result instanceof Promise)) {
-      result = Promise.resolve(result);
-    }
-
-    return result;
+    return getUserDataSource(callback).then(function resolveResult(data) {
+      return settings.write(key, JSON.stringify(data), ttl).then(function() {
+        return data;
+      });
+    });
   });
 }
 
 module.exports = {
-  constructCacheKey: constructCacheKey,
-  readthrough: readthrough
+  setup: setup,
+  readthrough: readthrough,
+  _constructCacheKey: constructCacheKey,
+  _getUserDataSource: getUserDataSource,
+  _settings: settings
 };
