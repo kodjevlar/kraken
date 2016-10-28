5471dc360c5dea6920e991f221866c8ec8ae95db
Johan Kanefur <johan.canefur@gmail.com>
2016-10-28 16:42:38 +0200

    Timer in runtime (#7)
    * Added runtime timer
    
    * Added more tests for coverage
    
    * Added newlines after variable declaration
    
    * Fixed typo

---
+++ src/lib/timing/index.spec.js
@@ -0,0 +1,110 @@
+const chai = require('chai');
+const expect = chai.expect;
+const sinon = require('sinon');
+
+const Timing = require('./index');
+
+describe('Profiling', function() {
+  describe('Report', function() {
+    it('should set callback function', function() {
+      const someCallback = function() {};
+      
+      Timing.report(someCallback);
+      expect(Timing._settings.reportHandler).to.equal(someCallback);
+    });
+
+    it('should set report level', function() {
+      const somelevel = 99;
+
+      Timing.report(function() {}, somelevel);
+      expect(Timing._settings.currentLevel).to.equal(somelevel);
+    });
+
+    it('should throw if callback isnt a function', function() {
+      expect(function() {
+        Timing.report('not a function', 5);
+      }).to.throw(TypeError);
+    });
+  });
+
+  describe('hrtimeToMs', function() {
+    it('should return a float', function(done) {
+      const start = process.hrtime();
+
+      setTimeout(function() {
+        const actual = Timing._hrtimeToMs(process.hrtime(start));
+
+        // Depending on the speed of the test environment
+        expect(actual).to.be.above(5);
+        expect(actual).to.be.below(15);
+        done();
+      }, 10);
+    });
+  });
+
+  describe('Check', function() {
+    it('should throw if error threshold is set lower than warn', function() {
+      expect(function() {
+        new Timing.Check('Do stuff', 10, 5);
+      }).to.throw(TypeError);
+    });
+
+    it('should always report on debug level', function(done) {
+      Timing.report(function() {
+        done();
+      }, Timing.levels.debug);
+
+      const check = new Timing.Check('some check', 100000, 100000);
+      check.stop();
+    });
+
+    it('should emit info if above threshold', function(done) {
+      Timing.report(function(level, actionName, actual, assertions) {
+        expect(level).to.equal(Timing.levels.info);
+        done();
+      });
+
+      const check = new Timing.Check('some check', 0, 1000);
+
+      check.stop();
+    });
+
+    it('should not emit report if below threshold', function() {
+      const spy = sinon.spy();
+
+      Timing.report(spy);
+
+      const check = new Timing.Check('some check', 1000, 1000);
+
+      check.stop();
+      expect(spy.callCount).to.equal(0);
+    });
+
+    it('should emit warning if above threshold', function(done) {
+      Timing.report(function(level, actionName, actual, assertions) {
+        expect(level).to.equal(Timing.levels.warn);
+        done();
+      });
+
+      const check = new Timing.Check('some check', -1, 0);
+      check.stop();
+    });
+
+    it('should emit info about timing', function(done) {
+      Timing.report(function(level, actionName, actual, assertions) {
+        expect(level).to.equal(Timing.levels.warn);
+        expect(actionName).to.equal('some check');
+        expect(actual).to.be.above(-1);
+        expect(actual).to.be.below(3);
+        expect(assertions).to.deep.equal({
+          info: -1,
+          warn: 0
+        });
+        done();
+      });
+
+      const check = new Timing.Check('some check', -1, 0);
+      check.stop();
+    });
+  });
+});
